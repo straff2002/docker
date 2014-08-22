7d1be55b76907c510596fe05d772df5b51c4e1c1
Felix Böhm <felixboehm@gmx.de>
2014-08-22 19:43:38 +0200

    docker tests

--- octest-docker.sh
+++ octest-docker.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-#Preparation
-#===========
+BASE_PATH=$(dirname $0)
+cd $BASE_PATH
+
+TESTS=$PWD/tests
+IMAGE_PATH=$PWD/images/owncloud-apache
+IMAGE_NAME="oc-apache"
+NAME="oc-test"
 
 #Get insecure key if not available
-if [[ ! -f configs/insecure_key ]]; then
+if [ ! -f configs/insecure_key ]; then
 	echo "Downloading insecure_key file for ssh connection"
 	curl -o configs/insecure_key -fSL https://github.com/phusion/baseimage-docker/raw/master/image/insecure_key
 	chmod 600 configs/insecure_key
 fi
 
+if [ $(docker images | grep $IMAGE_NAME -c ) -ne 0 ]
+then
+	echo "Image exists"
+else
+	echo "Building image"
+	docker build -t $IMAGE_NAME $IMAGE_PATH
+fi
+
+if [ $(docker ps -a | grep -c $NAME) -ne 0 ]
+then
+  docker stop $NAME
+  docker rm $NAME
+	echo "Restart test system"
+fi
+
 #Start OwnCloud-apache-Containerd
-docker run -dp 80:80 --name="ownCloud" oc-apache /sbin/my_init --enable-insecure-key
+docker run -dp 8888:80 -h $NAME --name=$NAME $IMAGE_NAME /sbin/my_init --enable-insecure-key
 
 #Get IP of ownCLoud-Server
-IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" ownCloud) 
+IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $NAME) 
 
-SERVER_DIR="/var/www/owncloud"
-
-TESTS=$(dirname $0)
-cd $TESTS
-TESTS=$PWD
-
-cd $TESTS
 protractor $TESTS/protractor_conf.js --params.baseUrl=$IP --suite install
 
 #cd $SERVER_DIR
@@ -32,5 +45,4 @@ protractor $TESTS/protractor_conf.js --params.baseUrl=$IP --suite install
 scp -i configs/insecure_key configs/disable_firstrunwizard.sh root@$IP:/tmp/disable_firstrunwizard.sh
 ssh -i configs/insecure_key root@$IP /tmp/disable_firstrunwizard.sh
 
-cd $TESTS
 protractor $TESTS/protractor_conf.js --params.baseUrl=$IP --suite login
